2d0f988f038b17de3a68c0d148a6657b4a3e103f
nkhlkp <168550564+nkhlkp@users.noreply.github.com>
2025-05-10 10:24:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,13 +33,13 @@ def sanitize_filename(filename):
 
 def download_best_audio_as_mp3(video_url, save_path=DOWNLOAD_DIR):
     ydl_opts = {
-        'ffmpeg_location': ffmpeg_path,  # Explicitly tell yt-dlp where to find ffmpeg
         'outtmpl': save_path + '/%(title)s.%(ext)s',  # Save path and file name
         'postprocessors': [{  # Post-process to convert to MP3
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',  # Convert to mp3
             'preferredquality': '0',  # '0' means best quality, auto-determined by source
         }],
+         'ffmpeg_location': "/usr/bin/ffmpeg"
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([video_url])
