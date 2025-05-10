3a4b843174819df624b46bbab0d069ae956f3589
nkhlkp <168550564+nkhlkp@users.noreply.github.com>
2025-05-10 09:20:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,9 @@ os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 DB_PATH = os.path.join(BASE_DIR, "fingerprint_database.pkl")
 MAP_PATH = os.path.join(BASE_DIR, "song_mapping.pkl")
 
+ffmpeg_path = ffmpeg.get_executable()
+os.environ["PATH"] += os.pathsep + os.path.dirname(ffmpeg_path)
+
 # ---------------------------------------------------------------------------------------------
 
 
